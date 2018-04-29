beed466c3bee3cc5004aa89de6b087bf0ea1e3b8
Guoshun Nan <nanguoshun@bupt.edu.cn>
2018-04-29 22:35:26 +0800

    update

---
+++ common.h
@@ -0,0 +1,21 @@
+//
+// Created by  ngs on 27/04/2018.
+//
+
+#ifndef CLIONTEST_COMMON_H
+#define CLIONTEST_COMMON_H
+
+const int LINE_MAX_SIZE = 8192;
+const int TAG_MAX_SIZE = 1024;
+const int DIGITAL_FLAG = 123456789;
+const double SMOOTH_VALUE = 0.0001;
+const std::string SPACE_OF_LINE ="\t";
+const std::string OUT_FLAG = "OUT";
+const std::string SPERATOR_FLAG = "%";
+const std::string TAGER_BIO_B = "B";
+const std::string TAGER_BIO_I = "I";
+const std::string TAGER_BIO_O = "O";
+const std::string NOT_FOUND_FLAG = "NOT_FOUND_FLAG";
+const std::string NOT_FOUND = "NOT_FOUND";
+const std::string PATH_START = "PATH_START";
+#endif //CLIONTEST_COMMON_H
